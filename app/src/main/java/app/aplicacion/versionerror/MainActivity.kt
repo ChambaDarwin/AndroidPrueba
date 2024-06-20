300ab6f85facc0e13c078d2bc11f9bd3510469ae
ChambaDarwin <chambaflores1999@gmail.com>
2024-06-20 18:39:09 -0500

    cambios

--- app/src/main/java/app/aplicacion/versionerror/MainActivity.kt
+++ app/src/main/java/app/aplicacion/versionerror/MainActivity.kt
@@ -2,10 +2,14 @@ package app.aplicacion.versionerror
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("trabajo 1")
+        println("trabajo 2")
+        println("trabajo 3")
     }
 }
